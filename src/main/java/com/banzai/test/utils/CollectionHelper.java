f741bd3bf1a84aa4f9397f9d183b492313384201
Кузнецов Александр Сергеевич <A.Kuznetsov@alfacapital.ru>
2018-12-04 08:53:17 +0300

    add CollectionHelper utility class, logback configuration, readme file

--- src/main/java/com/banzai/test/utils/CollectionHelper.java
+++ src/main/java/com/banzai/test/utils/CollectionHelper.java
@@ -8,13 +8,15 @@ import java.util.List;
  * Created by Kuznetsov A.S. 08.10.2018
  */
 public class CollectionHelper {
-    public static <T> List<List<T>> getChunkList(List<T> largeList , int chunkSize) {
+    public static <T> List<List<T>> getChunkList(final List<T> largeList , final int chunkSize) {
         if (largeList==null){
             return Collections.emptyList();
         }
         final List<List<T>> chunkList = new ArrayList<>();
         for (int i = 0 ; i <  largeList.size() ; i += chunkSize) {
-            chunkList.add(largeList.subList(i , i + chunkSize >= largeList.size() ? largeList.size() : i + chunkSize));
+            final boolean isLastChunk = (i + chunkSize) >= largeList.size();
+
+            chunkList.add(largeList.subList(i , isLastChunk? largeList.size() : i + chunkSize));
         }
         return chunkList;
     }
