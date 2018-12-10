1165f9b885598a5977c9c58313fe6c6433bc8046
Suntey <Suntey.kuznetsov@gmail.com>
2018-12-10 22:02:01 +0300

    little fix

--- src/test/java/com/banzai/test/service/ScheduledUploadServiceImplTest.java
+++ src/test/java/com/banzai/test/service/ScheduledUploadServiceImplTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 
@@ -23,6 +24,7 @@ public class ScheduledUploadServiceImplTest {
     @Autowired
     private EntryService entryService;
 
+    @Transactional
     @Test
     public void uploadFiles() throws InterruptedException {
         scheduledUploadService.uploadFiles();
